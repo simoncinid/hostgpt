913136f5abf5a3188a12bf05f21d860d08791006
simoncinid <simoncinidiego10@gmail.com>
2025-09-29 21:16:35 +0200

    d

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -63,8 +63,9 @@ export async function GET() {
 
   return new NextResponse(sitemap, {
     headers: {
-      'Content-Type': 'application/xml',
+      'Content-Type': 'application/xml; charset=utf-8',
       'Cache-Control': 'public, max-age=3600, s-maxage=3600',
+      'X-Robots-Tag': 'noindex',
     },
   })
 }
